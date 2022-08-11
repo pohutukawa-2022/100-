6be0a3e716b158a6f82a7b8db2d777e4f701e43c
Frey McCahon <freymccahonjones@gmail.com>
2022-08-11 19:05:12 +1200

    add non-meat food objs and image files

---
+++ food-data.js
@@ -0,0 +1,44 @@
+chewbit = {
+  id: '',
+  Image: '',
+  rarity: 'rare',
+  healthRating: 2,
+  ageMonths: 8,
+  poisonStatus: 'not poisonous',
+}
+
+grapefruit = {
+  id: '',
+  Image: '',
+  rarity: 'common',
+  healthRating: 4,
+  ageMonths: 0.5,
+  poisonStatus: 'not poisonous',
+}
+
+paint = {
+  id: '',
+  Image: '',
+  rarity: 'common',
+  healthRating: 0,
+  ageMonths: 15,
+  poisonStatus: 'poisonous',
+}
+
+seaweed = {
+  id: '',
+  Image: '',
+  rarity: 'uncommon',
+  healthRating: 5,
+  ageMonths: 4,
+  poisonStatus: 'not poisonous',
+}
+
+seeds = {
+  id: '',
+  Image: '',
+  rarity: 'common',
+  healthRating: 5,
+  ageMonths: 6,
+  poisonStatus: 'not poisonous',
+}
