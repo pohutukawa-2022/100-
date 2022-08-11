e795dc2ce8ee0d76045c3ca73582688214006fb2
livikavesi <livitaufao@gmail.com>
2022-08-11 21:25:02 +1200

    server, express & handlebars setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+
+const server = express()
+
+// Server configuration
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: false }))
+
+// Handlebars configuration
+server.engine('hbs', hbs.engine({ extname: 'hbs' }))
+server.set('view engine', 'hbs')
+
+
+
+
+module.exports = server
